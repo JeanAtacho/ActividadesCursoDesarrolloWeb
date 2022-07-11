ebf46f4992e61f88d7bd07e1297b071f7b915f07
JeanAtacho <107499872+JeanAtacho@users.noreply.github.com>
2022-07-11 20:17:43 +0200

    Avance al 11.07.22

---
+++ Actividades JEAN/Clase22, Semana5/Actividad1-WebCV/script.js
@@ -0,0 +1,8 @@
+ // Script para barra de menu hamburguesa//
+ function openNav(){
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.height = "0%";
+}
